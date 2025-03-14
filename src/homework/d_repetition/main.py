9dff9e8aae6bf8afa1991e9c5e0d7a4d037638c2
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-14 07:44:01 +0000

    Homework 3 corrected

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,24 @@
 #
+def main():
+
+    def get_factorial(num):
+             for i in range(2, num + 1):
+                   result *= i
+
+                   return result
+
+
+def sum_odd_numbers(num):
+      
+    total = 0
+    odds = 1
+
+    while odds <= num:
+         total += odds
+         odds += 2
+         print (total, odds)
+        
+    return total
+
+main()
+
