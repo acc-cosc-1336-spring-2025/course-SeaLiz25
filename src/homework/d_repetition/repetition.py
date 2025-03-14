c8dca6da6b23e780dc8fe591d47f64c7a1da5911
SeaLiz25 <Elizabeth.rodriguez5@g.austincc.edu>
2025-03-14 07:36:39 +0000

    Homework 3 corrected

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,20 @@
 #
+
+def get_factorial(num):
+    for i in range(2, num + 1):
+            result *= i
+            return result
+
+
+def sum_odd_numbers(num):
+      
+    total = 0
+    odds = 1
+
+    while odds <= num:
+         total += odds
+         odds += 2
+         print (total, odds)
+        
+    return total
+  
--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,1 +1,17 @@
 #
+import unittest
+from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition.repetition import sum_odd_numbers
+class Test_Config(unittest.TestCase):
+
+    def test_get_factorial(self):
+        self.assertEquals (get_factorial(4),24)
+        self.assertEquals (get_factorial(5),120)
+        self.assertEquals (get_factorial(6),120)
+
+    def test_sum_odd_numbers(self):
+        self.assertEquals (sum_odd_numbers(7), 15)
+        self.assertEquals (sum_odd_numbers(9), 25)
+        self.assertEquals (sum_odd_numbers(10), 25)
+
+
